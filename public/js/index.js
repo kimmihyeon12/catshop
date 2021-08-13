751b30dda294b063d6396539fb02a0b90cc7e477
mihyeon <2_5_3_1@naver.com>
2021-08-13 16:24:57 +0900

    first

---
+++ public/js/index.js
@@ -0,0 +1,55 @@
+console.log("index.js");
+const catagory =document.querySelector(".catagory-wrap");
+const topBanner =document.querySelector(".top-banner");
+const sliderContainer =document.querySelector(".slider-container");
+const slideWidth = window.innerWidth;
+const sliderlength = document.querySelectorAll('.slider-container img').length;
+let count = 0;
+let index = 0;
+const pageicon =document.querySelectorAll(".pagination span");
+
+document.addEventListener('scroll', () => {
+ console.log(window.scrollY);
+ if(window.scrollY>=60){
+     catagory.style.position=`fixed`;
+     topBanner.style.height=`0px`;
+     catagory.style.backgroundColor=`white`;
+     catagory.style.borderBottom=`1px lightgrey solid`;
+ }else{ 
+    catagory.style.position=`unset`;
+    topBanner.style.height=`50px`;
+    catagory.style.backgroundColor=`transparent`;
+    catagory.style.borderBottom=`1px transparent solid`;
+    
+ }
+    });
+
+  
+    setInterval(function() {
+        console.log(index);
+        for(let i=0; i<pageicon.length; i++){
+            if(i==index){
+                console.log(index);
+                pageicon[i].style.width=`20px`;
+                pageicon[i].style.backgroundColor=`black`;
+            }
+            else{
+                pageicon[i].style.width=`8px`;
+                pageicon[i].style.backgroundColor=`#676363`;
+            }
+        }
+        count++;
+        index++;
+        if(index == 3){
+            index=0;
+        }
+        
+         sliderContainer.style.transform = `translate3d(-` + (slideWidth * count) + `px, 0px, 0px)`;
+       
+         if(count==2){
+             
+            count=-1;
+         }
+    }, 3000);
+
+ 
